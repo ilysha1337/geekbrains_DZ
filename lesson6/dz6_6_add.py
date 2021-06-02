005732eb69e4f4c79875de5926b2f062614c0f6a
ilysha1337 <kittenwow1992@mail.ru>
2021-06-02 21:33:57 +0300

    dz on lesson 6

--- lesson6/dz6_6_add.py
+++ lesson6/dz6_6_add.py
@@ -7,7 +7,7 @@ with open("bakery.csv", 'a', encoding='utf-8') as f_add:
                 first, lost = map(int, argv[1:])
                 print(*(tmp for i, tmp in enumerate(f_read) if first - 1 <= lost), sep='')
             if ',' in argv[1] or '.' in argv[1]:
-                if round(float(argv[1].replace(',', '.')), 3) <= 99999,99:
+                if round(float(argv[1].replace(',', '.')), 3) <= 99999.99:
                     print(f"{round(float(argv[1].replace(',', '.')), 3):>010}", file=f_add)
                 else:
                     print('Сумма должна быть меньше 99999,99!')
